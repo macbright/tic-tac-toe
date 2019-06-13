43ad0f6ff9d2349bbb39824db0fa6dac6aecd2e0
bright okike <brightokike@gmail.com>
2019-06-13 12:25:25 +0300

    the game_board editted

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,5 +1,3 @@
-require_relative 'player.rb'
-
 class GameBoard
 
     attr_reader :board
@@ -7,11 +5,11 @@ class GameBoard
     def initialize
         @board = Array.new(3){Array.new(3)}
         board_layout(@board)
-        @turn = 0
-        @player = Player.new()
-        @move_animation = @player.set_mark
+ 
     end
-    
+
+    public 
+
     def board_layout(board)
         puts " #{board[0][0]|| 1}  |  #{board[0][1] || 2}  |  #{board[0][2] || 3} "  
         puts "----+-----+----" 
@@ -20,10 +18,13 @@ class GameBoard
         puts " #{board[2][0] ||7}  |  #{board[2][1] ||8}  |  #{board[2][2] ||9} "
     end
 
-    def move(player_action)
+    def reset_board 
+        GameBoard.new()
+    end
+
+    def move(player_action, mark)
         if !position_taken(player_action)
-            @board[player_action[0]][player_action[1]] = @move_animation
-            animation_current_turn
+            @board[player_action[0]][player_action[1]] = mark
             print board_layout(@board)
             print "\n"
         else
@@ -31,22 +32,30 @@ class GameBoard
         end
     end
 
-    def reset_board 
-        GameBoard.new()
+    def our_hash
+        new_hash = {
+            1 => [0,0],
+            2 => [0,1],
+            3 => [0,2],
+            4 => [1,0],
+            5 => [1,1],
+            6 => [1,2],
+            7 => [2,0],
+            8 => [2,1],
+            9 => [2,2]
+        }
+        return new_hash
     end
+
 
     private 
 
-    def animation_current_turn
-        @move_animation == "X" ?  @move_animation = "O" : @move_animation = "X" if @turn%2==0 
-        @turn = 2
-    end
-
-    def position_taken(player_action)
-        if @board[player_action[0]][player_action[1]] != nil
+    def position_taken(player)
+        coordinates = player
+        if @board[coordinates[0]][coordinates[1]] != nil
             return true
         end
     end
 
-end
 
+end
